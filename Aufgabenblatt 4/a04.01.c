93c966f8d2804cc235aff218693bad3fbedfa9d0
Daniel Bustamante <daniel.bustamante@student.hs-rm.de>
2024-12-09 23:51:23 +0100

    dkoasdfk

--- Aufgabenblatt 4/a04.01.c
+++ Aufgabenblatt 4/a04.01.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h> /*for malloc, free, exit*/
+#include <string.h> /*for strncpy*/
+
 typedef struct Node{ /*structs is like a class without methods*/
     char name[26];   /*typedef creates a name for a datatype*/
     struct Node* next;
@@ -7,17 +10,18 @@ typedef struct Node{ /*structs is like a class without methods*/
 int main(int argc, char* argv[]){
     Node* head = NULL; /*head must be initialized*/
     Node* tail = NULL; /*tail always point to the last node of the list*/
-    char input[26];
+    char input[25];
 
     printf("Geben Sie die Namen ein:");
     /*scanf recognizes the separators automatically (' ',\t, \n) */
     while(scanf("%25s", &input) != EOF){
         Node* new_node = malloc(sizeof(Node)); /*allocate the node in a memory space*/
         if(!new_node){ 
+            perror("Error assigning memory");
             return 1;
         }
         /*char* strcpy(destiny name, origin name)*/
-        strcpy(new_node->name, input);
+        strncpy(new_node->name, input, 25);
 
         if(head == NULL){
             head = new_node;
