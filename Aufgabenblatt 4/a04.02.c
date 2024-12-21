140f29327e5635603e6cf6f962551d8db6bff7f9
Daniel Bustamante <daniel.bustamante@student.hs-rm.de>
2024-12-21 18:17:26 +0100

    a04.02

--- Aufgabenblatt 4/a04.02.c
+++ Aufgabenblatt 4/a04.02.c
@@ -6,22 +6,30 @@ typedef struct Node{
     char *data;
 }Node;
 
-typedef Node *nodep; /*pointer to the struct*/
+typedef Node *nodep; /*nodep is an alias for Node* (and pointer to Node*) */
 
 void printList(nodep lst){
     nodep current = lst;
     while(current != NULL){
         printf("Adress: %p Content: %s\n", current, current->data);
-        current =  current->next;
+        current = current->next;
     }
 }
 
 nodep insertAt (nodep lst, int pos, char *inhalt){
-    Node *head = NULL;
-    Node *tail = NULL;
-    head = lst;
-    if (!head){
+    nodep new_node = malloc(sizeof(Node)); /* nodep new_node => Node* new_node */
+    new_node->data = inhalt;
+    new_node->next = NULL;
+    new_node->prev = NULL;
 
+    if (lst == NULL){
+        return new_node;
+    }
+    
+    if(pos == 0){
+        new_node->next = lst;
+        new_node->prev = lst;
+        return new_node; /*heree*/
     }
 }
 
