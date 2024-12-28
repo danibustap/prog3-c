41788c80fe3c7b9ebd849630cea60b940a5847f7
Daniel Bustamante <daniel.bustamante@student.hs-rm.de>
2024-12-28 13:32:08 +0100

    a04.02.ccc

--- Aufgabenblatt 4/a04.02.c
+++ Aufgabenblatt 4/a04.02.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct Node{
     struct Node *next;
@@ -17,7 +18,7 @@ void printList(nodep lst){
 }
 
 nodep insertAt (nodep lst, int pos, char *inhalt){
-    nodep new_node = malloc(sizeof(Node)); /* nodep new_node => Node* new_node */
+    nodep new_node = malloc(sizeof(Node)); /* nodep new_node it's equivalent to Node* new_node. malloc always must be freed with "free"*/
     new_node->data = inhalt;
     new_node->next = NULL;
     new_node->prev = NULL;
@@ -27,13 +28,41 @@ nodep insertAt (nodep lst, int pos, char *inhalt){
     }
     
     if(pos == 0){
+        if(lst != NULL){
+            lst->prev = new_node;
+        }
         new_node->next = lst;
-        new_node->prev = lst;
-        return new_node; /*heree*/
+        return new_node; /*new_node is now the start node*/
+    } else if (pos == -1){
+        nodep current = lst; /*first known node in the list*/
+        while(current->next != NULL){
+            current = current->next;
+        }
+        current->next = new_node;
+        new_node->prev = current;
+        return lst; /*always return first node*/
     }
 }
 
+void freeList(nodep lst){
+    nodep current = lst;
+    while(current != NULL){
+        nodep next = current->next;
+        free(current);
+        current = next;
+    }
+}
+
+nodep deleteAt (nodep lst, int pos){
+
+}
+  
 int main(){
     nodep list = NULL;
+    list = insertAt(list, 0, "hola soy un nodo");
+    list = insertAt(list, -1, "soy el nodo 2");
     printList(list);
+
+    freeList(list);
+    return 0;
 }
